d7f8e82de95ca135a5139dcb822aa84bd6040f03
Vignesh Sattanathan <vignesh.sattanathan@ingka.ikea.com>
2025-10-19 09:56:23 +0200

    adding page not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,18 @@
-import { FeedbackComponent } from './feedback/feedback.component';
+import {FeedbackComponent} from './feedback/feedback.component';
 import {DcfCalculatorComponent} from "./dcf-calculator/dcf-calculator.component";
 import {Routes, RouterModule} from "@angular/router";
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
+import {PageNotFound} from "./page-not-found/page-not-found";
 
 const routes: Routes = [
     {path: '', component: DcfCalculatorComponent},
-    { path: 'feedback', component: FeedbackComponent }
+    {path: 'feedback', component: FeedbackComponent},
+    {path: '**', component: PageNotFound}
 ];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
     exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
